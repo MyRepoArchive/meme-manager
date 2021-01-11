e104bf30836c9f3ba835391d997da09e89241470
João Victor <joaovictor-san-co@hotmail.com>
2021-01-11 10:28:41 -0300

    getting prefix

--- src/events/message/index.ts
+++ src/events/message/index.ts
@@ -1,7 +1,13 @@
+import { Guild, IGuildDb } from "../../models/Guilds";
 import { Event } from "../../structures/Event";
 
-const event = new Event('discord_client', 'message', (client, message) => {
-  const prefix = client.defaultPrefix;
+const event = new Event('discord_client', 'message', async (client, message) => {
+  if (message.channel.type === 'dm') return;
+
+  const prefixDb = ((await Guild.findOne({ guild_id: message.guild!.id })) as IGuildDb | null)?.prefix;
+  const prefix = prefixDb || client.defaultPrefix;
+  
+  if (!prefixDb) new Guild({ guild_id: message.guild!.id }).save();
 });
 
 export { event };
